ecd84858b55ea26f3bd99add7283bd062399b3bb
Ucl.org.ru <vauglev@gmail.com>
2020-10-11 20:28:22 +0700

    Add files via upload

---
+++ src/show.py
@@ -0,0 +1,29 @@
+from tinydb import *
+from tinydb.operations import *
+from tinydb import Query
+
+from datetime import *
+
+dbtb = TinyDB('./dbtb.json')
+Tb= Query()
+
+for it in dbtb.all():
+    print(it.doc_id, it['name'],it['theme'],it['date'],it['time'],it['delta'],it['mood'],it['xn'] )
+
+ch=input(" >")
+ 
+res = dbtb.get(doc_id=int(ch))
+
+print (res['theme'])
+print (res['date'])
+print (res['time'])
+print (res['mood'])
+
+a=datetime.strptime(res["date"]+" "+res["time"], "%d-%m-%y %H:%M") # создание обьекта время
+dt=timedelta(minutes=int(res["delta"])) # обьект тайм дельта в минутах
+b=datetime.now() # момент времени соответвующий сейчас
+c=a+dt # момент времени окончания интервала
+ba=b-a # удалим из настоящего момента время до начала интервала оставив то что прошло
+pro= (100*ba.total_seconds())/dt.total_seconds() # процент времени который прошел
+print ("%",pro)
+print ("Окончание",c)
